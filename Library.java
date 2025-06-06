1616d45b06c1029a0205f572c498483e9afff8e5
Prabakaran-web <suryaprabaaero@gmail.com>
2025-06-06 12:29:31 +0530

    Add files via upload
    Home assignments 31st May

---
+++ Library.java
@@ -0,0 +1,29 @@
+package week2.day1;
+
+public class Library {
+
+	public String addBook(String bookTitle) {
+		
+		System.out.println("Book added successfully");
+		return "bookTitle";
+		
+		
+	}
+	
+	public void issueBook()
+	{
+		System.out.println("Book issued successfully");
+	}
+	public static void main(String[]args)
+	
+	{
+		Library books=new Library();
+		
+		books.addBook("bookTitle");
+		books.issueBook();
+	}
+
+	
+		
+	}
+
---
+++ LibraryManagement.java
@@ -0,0 +1,11 @@
+package week2.day1;
+
+public class LibraryManagement {
+
+	public static void main(String[]args)
+	{
+		Library books=new Library();
+	
+	books.issueBook();
+	books.addBook("bookTitle");
+}}
